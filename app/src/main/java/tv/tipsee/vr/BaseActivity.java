3d92c69466d8f86b14313344e1838b39ebf8bc3c
unknown <>
2016-06-20 15:43:39 +0800

    Initial commit

---
+++ app/src/main/java/tv/tipsee/vr/BaseActivity.java
@@ -0,0 +1,44 @@
+package tv.tipsee.vr;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.umeng.analytics.MobclickAgent;
+
+/**
+ * Created by liqy on 2016/6/20.
+ */
+public class BaseActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        MobclickAgent.onResume(this);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        MobclickAgent.onPause(this);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
